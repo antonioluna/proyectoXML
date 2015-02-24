f041907fa360fff763c4d5927165c8962f1a24b1
Antonio Luna <antonio__luna@outlook.es>
2015-02-24 21:47:20 +0100

    añadiendo código para abrir los archivos

--- xml.py
+++ xml.py
@@ -9,3 +9,19 @@ from lxml import etree
 with open("./DATA/controles-alcoholemia.xml", "r")as alcohol:
     alcoholr = etree.parse(alcohol)
 
+with open("./DATA/curvas-peligrosas.xml", "r")as curvas:
+    curvasr = etree.parse(curvas)
+
+with open("./DATA/puntos-negros.xml", "r")as negros:
+    negrosr = etree.parse(negros)
+
+with open("./DATA/radares-camuflados.xml", "r")as camuflados:
+    camufladosr = etree.parse(camuflados)
+
+with open("./DATA/radares-fijos.xml", "r")as fijos:
+    fijosr = etree.parse(fijos)
+
+
+
+
+
