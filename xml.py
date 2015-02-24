47403334ea25db62c6751a3a9c8ba2bea0816264
Antonio Luna <antonio__luna@outlook.es>
2015-02-24 21:41:23 +0100

    añadiendo código para abrir los archvos

--- xml.py
+++ xml.py
@@ -1,3 +1,11 @@
 #-*- coding: utf-8 -*-
 
 from lxml import etree
+
+#def alcoholemia(valor):
+
+#Abrimos los ficheros con los datos
+
+with open("./DATA/controles-alcoholemia.xml", "r")as alcohol:
+    alcoholr = etree.parse(alcohol)
+
